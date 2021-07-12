5b71ae39e335a91f8607487e12814e2c05912978
atefeh <hemmati.atefeh97@gmail.com>
2021-07-12 20:19:10 +0430

    first

---
+++ content/js/main.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function() {
+	$(".hero-slider").slick({    
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1800,
+        speed: 800,
+        dots: true,
+        infinite: true,
+        variableWidth: true,
+	});
+	
+    });
