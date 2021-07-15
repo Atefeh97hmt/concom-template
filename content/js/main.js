005f0ba52034f3dc5fc42933a512ccab0cfde812
atefeh <hemmati.atefeh97@gmail.com>
2021-07-15 19:52:30 +0430

    2 parts added

--- content/js/main.js
+++ content/js/main.js
@@ -1,44 +1,49 @@
-﻿$(window).on("scroll", function() {
-  if($(window).scrollTop() > 65) {
-      $(".main-header").addClass("active");
+﻿////// menu scroll //////////
+
+$(window).on("scroll", function () {
+  if ($(window).scrollTop() > 65) {
+    $(".main-header").addClass("active");
   } else {
-     $(".main-header").removeClass("active");
+    $(".main-header").removeClass("active");
   }
 });
 
 
-$(document).ready(function() {
-  $(".partner-slider").slick({    
+////// sliders //////////
+
+$(document).ready(function () {
+  $(".partner-slider").slick({
     slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 1800,
     speed: 800,
     dots: false,
-    arrows:false,
-});
-	$(".hero-slider").slick({    
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1800,
-        speed: 800,
-        dots: true,
-        arrows:false,
-        centerMode: true,
-        centerPadding: '0px',
-
+    arrows: false,
   });
-  
- 
-	
-    });
-    var scrollToTopBtn = document.getElementById("toTop")
-    var rootElement = document.documentElement
-    function scrollToTop() {
-      rootElement.scrollTo({
-        top: 0,
-        behavior: "smooth"
-      })
-    }
-    scrollToTopBtn.addEventListener("click", scrollToTop)
+  $(".hero-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1800,
+    speed: 800,
+    dots: true,
+    arrows: false,
+    centerMode: true,
+    centerPadding: '0px',
+  });
+});
+
+
+////// scrollToTopBtn //////////
+
+var scrollToTopBtn = document.getElementById("toTop")
+var rootElement = document.documentElement
+
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop)
