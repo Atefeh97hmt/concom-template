c7bd46d19d380006ae6d4c4e28dbf4c841dce475
atefeh97hmt <hemmati.atefeh97@gmail.com>
2021-07-19 13:15:17 +0430

    effects

--- content/js/main.js
+++ content/js/main.js
@@ -1,67 +1,82 @@
 ﻿////// menu scroll //////////
 
-$(window).on("scroll", function () {
-  if ($(window).scrollTop() > 65) {
-    $(".main-header").addClass("active");
-  } else {
-    $(".main-header").removeClass("active");
-  }
+$(window).on("scroll", function() {
+    if ($(window).scrollTop() > 65) {
+        $(".main-header").addClass("active");
+    } else {
+        $(".main-header").removeClass("active");
+    }
 });
 
 
 ////// sliders //////////
 
-$(document).ready(function () {
-  $(".hero-slider").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1800,
-    speed: 800,
-    dots: true,
-    dotsClass: 'slide-dots',
-    arrows: false,
-    centerMode: true,
-    centerPadding: '0px',
-  });
-  $(".partner-slider").slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1800,
-    speed: 800,
-    dots: false,
-    arrows: false,
-    responsive: [{
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 1800,
-          speed: 800,
-          dots: false,
-          arrows: false,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 1800,
-          speed: 800,
-          dots: false,
-          arrows: false,
-        }
-      }
-    ]
+$(document).ready(function() {
+    $(".hero-slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1800,
+        speed: 800,
+        dots: true,
+        dotsClass: 'slide-dots',
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0px',
+    });
 
-  });
+    $(".customer-slider").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1800,
+        speed: 800,
+        dots: false,
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0px',
+    });
+    $(".partner-slider").slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1800,
+        speed: 800,
+        dots: false,
+        arrows: false,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 1800,
+                    speed: 800,
+                    dots: false,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 1800,
+                    speed: 800,
+                    dots: false,
+                    arrows: false,
+                }
+            }
+        ]
+
+    });
 
 });
-
+// $('.customer-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+//     $(".slick-slide").removeClass('works');
+//     $('.slick-current').addClass('works');
+// });
 
 ////// scrollToTopBtn //////////
 
@@ -69,9 +84,9 @@ var scrollToTopBtn = document.getElementById("toTop")
 var rootElement = document.documentElement
 
 function scrollToTop() {
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  })
+    rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
 }
 scrollToTopBtn.addEventListener("click", scrollToTop)
